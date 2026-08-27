1eae226261eceb8b3d15a4d86bf77bdcac944105
daryl-peterson <daryl.peterson@gmail.com>
2026-08-27 12:17:42 -0500

    new file:   Libs/DeviceImporter.php new file:   Page.php new file:   Settings.php new file:   resources/views/device-overview.blade.php new file:   resources/views/menu.blade.php new file:   resources/views/page.blade.php new file:   resources/views/port-tab.blade.php new file:   resources/views/settings.blade.php

---
+++ Settings.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Class device importer settings
+ *
+ * @package     App\Plugins\DeviceImporter
+ * @author      Daryl Peterson <@gmail.com>
+ * @copyright   Copyright (c) 2026, Daryl Peterson
+ * @license    https://opensource.org MIT License
+ * @link       https://github.com/daryl-peterson/librenms-device-importer/
+ * @since       1.0.0
+ */
+namespace App\Plugins\DeviceImporter;
+
+use App\Plugins\Hooks\SettingsHook;
+
+/**
+ * Class device importer settings
+ *
+ * @package     App\Plugins\DeviceImporter
+ * @author      Daryl Peterson <@gmail.com>
+ * @copyright   Copyright (c) 2026, Daryl Peterson
+ * @license    https://opensource.org MIT License
+ * @link       https://github.com/daryl-peterson/librenms-device-importer/
+ * @since       1.0.0
+ */
+class Settings extends SettingsHook {
+
+
+}
