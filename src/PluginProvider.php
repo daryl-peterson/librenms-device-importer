83c60a2a1a8959d433ecd7b090b6f289bcf580e1
daryl-peterson <daryl.peterson@gmail.com>
2026-09-10 11:08:14 -0500

    Updating

--- src/PluginProvider.php
+++ src/PluginProvider.php
@@ -14,13 +14,13 @@
 namespace DRP\DeviceImporter;
 
 
+use DRP\DeviceImporter\DbCheck;
 use DRP\DeviceImporter\Hooks\DeviceOverview;
 use DRP\DeviceImporter\Hooks\Menu;
 use DRP\DeviceImporter\Hooks\Page;
 use DRP\DeviceImporter\Hooks\Settings;
-use DRP\DeviceImporter\DbCheck;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Log;
-//use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use LibreNMS\Interfaces\Plugins\Hooks\MenuEntryHook as MenuEntryHookInterface;
 use LibreNMS\Interfaces\Plugins\Hooks\SettingsHook as SettingsHookInterface;
@@ -71,5 +71,35 @@ class PluginProvider extends ServiceProvider {
         $pluginManager->publishHook($pluginName, MenuEntryHookInterface::class, Menu::class);
         $pluginManager->publishHook($pluginName, SinglePageHook::class, Page::class);
         $pluginManager->publishHook($pluginName, SettingsHookInterface::class, Settings::class);
+
+        $this->clearCacheOnFirstRun();
+    }
+
+    /**
+     * Clear cache on the first run of the plugin.
+     *
+     * This method checks for a hidden lock file to determine if it's the first run.
+     * If it is, it clears the route, view, and cache, then creates the lock file.
+     */
+    protected function clearCacheOnFirstRun() {
+        // Path to a hidden lock file inside your plugin folder
+        $lockFile = __DIR__ . '/.installed';
+
+        // If the file doesn't exist, this is the first run
+        if (!file_exists($lockFile)) {
+            try {
+                // Programmatically run the equivalent of your 'lnms' commands
+                Artisan::call('route:clear');
+                Artisan::call('view:clear');
+                Artisan::call('cache:clear');
+
+                // Create the lock file so this code never triggers again
+                file_put_contents($lockFile, date('Y-m-d H:i:s'));
+
+                Log::info('Device Importer: First-run cache clear executed successfully.');
+            } catch (\Exception $e) {
+                Log::error('Device Importer: Failed to auto-clear cache: ' . $e->getMessage());
+            }
+        }
     }
 }
